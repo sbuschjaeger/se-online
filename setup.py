2ce2afc54e5f8347e4994519b55369eadebce700
Sebastian Buschjäger <sebastian.buschjaeger@tu-dortmund.de>
2021-02-18 15:23:11 +0100

    removed C++ part from setup / environment for now

--- setup.py
+++ setup.py
@@ -8,60 +8,59 @@ from distutils.version import LooseVersion
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
+# class CMakeExtension(Extension):
+#     def __init__(self, name, sourcedir=''):
+#         Extension.__init__(self, name, sources=[])
+#         self.sourcedir = os.path.abspath(sourcedir)
 
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
 
+# class CMakeBuild(build_ext):
+#     def run(self):
+#         try:
+#             out = subprocess.check_output(['cmake', '--version'])
+#             print(out)
+#         except OSError:
+#             raise RuntimeError("CMake must be installed to build the following extensions: " +
+#                                ", ".join(e.name for e in self.extensions))
 
-class CMakeBuild(build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(['cmake', '--version'])
-            print(out)
-        except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
+#         if platform.system() == "Windows":
+#             cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+#             if cmake_version < '3.1.0':
+#                 raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.1.0':
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+#         for ext in self.extensions:
+#             self.build_extension(ext)
 
-        for ext in self.extensions:
-            self.build_extension(ext)
+#     def build_extension(self, ext):
+#         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+#         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+#                       '-DPYTHON_EXECUTABLE=' + sys.executable]
 
-    def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+#         self.debug = False
+#         cfg = 'Debug' if self.debug else 'Release'
+#         build_args = ['--config', cfg]
 
-        self.debug = False
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
+#         env = os.environ.copy()
+#         if platform.system() == "Windows":
+#             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+#             if sys.maxsize > 2 ** 32:
+#                 cmake_args += ['-A', 'x64']
+#             build_args += ['--', '/m']
+#         else:
+#             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+#             # CMake sometimes does not really respect the compiler we are using (e.g. if installed inside a conda environment)
+#             # So lets make sure we use $CC / $CXX which is set appropriatley by conda
+#             cmake_args += ['-DCMAKE_C_COMPILER=' + env['CC']]
+#             cmake_args += ['-DCMAKE_CXX_COMPILER=' + env['CXX']]
+#             build_args += ['--', '-j', str(multiprocessing.cpu_count())]
 
-        env = os.environ.copy()
-        if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            if sys.maxsize > 2 ** 32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            # CMake sometimes does not really respect the compiler we are using (e.g. if installed inside a conda environment)
-            # So lets make sure we use $CC / $CXX which is set appropriatley by conda
-            cmake_args += ['-DCMAKE_C_COMPILER=' + env['CC']]
-            cmake_args += ['-DCMAKE_CXX_COMPILER=' + env['CXX']]
-            build_args += ['--', '-j', str(multiprocessing.cpu_count())]
+#         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+#                                                               self.distribution.get_version())
+#         if not os.path.exists(self.build_temp):
+#             os.makedirs(self.build_temp)
 
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.', '--target', 'PyBPE'] + build_args, cwd=self.build_temp)
+#         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+#         subprocess.check_call(['cmake', '--build', '.', '--target', 'PyBPE'] + build_args, cwd=self.build_temp)
 
 setup(
     name='PyBPE',
@@ -70,7 +69,8 @@ setup(
     author_email='sebastian.buschjaeger@tu-dortmund.de',
     description='Biased Prox Ensemble for Python (PyBPE)',
     long_description='',
-    ext_modules=[CMakeExtension('PyBPE')],
-    cmdclass=dict(build_ext=CMakeBuild),
+    packages=['PyBPE'],
+    # ext_modules=[CMakeExtension('PyBPE')],
+    # cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False
 )
